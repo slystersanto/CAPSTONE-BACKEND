9d58bbd52695dd9a20fa4dad95025fd7f20fb391
slystersanto <santhosh.mech.19@gmail.com>
2023-04-09 14:06:37 +0530

    cors domain updated

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000","https://comforting-semifreddo-525042.netlify.app/"],
   })
 );
 
