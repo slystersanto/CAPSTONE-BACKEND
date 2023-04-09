2fd4cb201b9f4be67773d7b00528ba440e409212
slystersanto <santhosh.mech.19@gmail.com>
2023-04-09 20:11:15 +0530

    sandy

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const cors = require("cors");
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://precious-mermaid-2319d8.netlify.app');
+   
+    res.setHeader('Access-Control-Allow-Origin', 'https://precious-mermaid-2319d8.netlify.app','http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
